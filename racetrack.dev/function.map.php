8809b5de0f75be3be2f0ee13e403fe31d982f9be
ec1:ben <ec1@a74.fr>
2016-02-25 10:05:36 +0100

    -

--- racetrack.dev/function.map.php
+++
@@ -1,5 +0,0 @@
-<?
-require_once"../rt/prepend.php";#IF not autoloaded via php auto_prepend_file OR .htaccess
-#require_once"../rt/lib/f.php";$f=new f;#global functions
-$x=new funmap;
-print_r($x);die;
--- racetrack.dev/test/index.php
+++ racetrack.dev/test/index.php
@@ -7,4 +7,5 @@ foreach($x as $v){
   if(strpos($v,'index.php')!==false or strpos($v,'.c.php')!==false or strpos($v,'.inc.php')!==false || strpos($v,'todo')!==false || strpos($v,'router')!==false)continue;
   echo"\n - <a href='$v'>".str_replace('.php','',$v)."</a>";
 }
+echo'</pre>';
 require_once'../footer.c.php';
