669a6202c812c73e964625103f79b3a59fac37b4
e530 <e530@a74.fr>
2016-02-28 19:42:33 +0100

    set Analytics

--- racetrack.dev/footer.c.php
+++ racetrack.dev/footer.c.php
@@ -1,4 +1,4 @@
 <div id=footermenu class=w50><?=$footermenu?><a href='/contact.php'>Contact</a></div>
 <div id=footer><?=$footer?></div>
-<script>setAnalytics(<?=$ga?>);</script>
+<script>var ga='<?=$ga?>';if(ga)setAnalytics(ga);</script>
 </body></html>
