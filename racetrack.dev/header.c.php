8a2ca208dbde01f7e686401d640713e398362741
ec1:ben <ec1@a74.fr>
2016-02-29 10:53:30 +0100

    analytics

--- racetrack.dev/header.c.php
+++ racetrack.dev/header.c.php
@@ -1,9 +1,21 @@
 <!DOCTYPE HTML><html><head><title><?=$_ENV['titre']?></title><meta name=description value="<?=$_ENV['desc']?>"><meta name=keywords value="<?=$_ENV['keyw']?>"><meta name="viewport" content="width=device-width, minimum-scale=0.1, maximum-scale=1.0"><meta http-equiv='Content-Type' content='text/html;charset=UTF8<?#ISO-8859-1?>;'><meta name='robots' content='noarchive'><link rel="icon" type="image/png" href="/favicon.png"><?=$header?>
 <script src='/?js<?=$jsh#!c/js.php?js,jq19,jquery-ui.min,jquery.bxslider.min,img,textshadow,blur.min,?>'></script>
 <script>var Analytics='<?=$ga?>';
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create',Analytics, 'auto');ga('send','pageview');
+/*
+
+var _gaq=[];_gaq.push(['_setAccount','UA-939677-48']);_gaq.push(['_setAllowAnchor',true]);_gaq.push(['_trackPageview']);
+s=document.createElement("script");document.head.appendChild(s);s.src='//google-analytics.com/ga.js';s.type='text/javascript';
+
 _gaq.push(['_setAllowAnchor',true]);_gaq.push(['_trackPageview']);_gaq.push(['_setAccount',Analytics]);
+
+
+
+
 addjs('//google-analytics.com/ga.js',function(){clog('ga:loaded');ga('create',Analytics,'auto');ga('send', 'pageview');},1);
 //setAnalytics('<?=$ga?>');
+*/
 </script>
 
 <link href="/?css<?#/!c/jquery.bxslider.css?>" rel="stylesheet">
