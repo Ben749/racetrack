93c77c2d7c0be47b079ca4c8d56709060874c472
ec1:ben <ec1@a74.fr>
2016-02-29 10:43:41 +0100

    analytics

--- racetrack.dev/footer.c.php
+++ racetrack.dev/footer.c.php
@@ -1,4 +1,3 @@
 <div id=footermenu class=w50><?=$footermenu?><a href='/contact.php'>Contact</a></div>
 <div id=footer><?=$footer?></div>
-<script>var Analytics='<?=$ga?>';if(Analytics)setAnalytics(Analytics);</script>
 </body></html>
--- racetrack.dev/header.c.php
+++ racetrack.dev/header.c.php
@@ -1,5 +1,6 @@
 <!DOCTYPE HTML><html><head><title><?=$_ENV['titre']?></title><meta name=description value="<?=$_ENV['desc']?>"><meta name=keywords value="<?=$_ENV['keyw']?>"><meta name="viewport" content="width=device-width, minimum-scale=0.1, maximum-scale=1.0"><meta http-equiv='Content-Type' content='text/html;charset=UTF8<?#ISO-8859-1?>;'><meta name='robots' content='noarchive'><link rel="icon" type="image/png" href="/favicon.png"><?=$header?>
 <script src='/?js<?=$jsh#!c/js.php?js,jq19,jquery-ui.min,jquery.bxslider.min,img,textshadow,blur.min,?>'></script>
+<script>setAnalytics('<?=$ga?>');</script>
 
 <link href="/?css<?#/!c/jquery.bxslider.css?>" rel="stylesheet">
 <link rel='stylesheet' type='text/css' href='//fonts.googleapis.com/css?family=Raleway'>
