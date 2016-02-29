5af2a0db4d12752e53cb2192584b57fbf056ee00
ec1:ben <ec1@a74.fr>
2016-02-29 15:17:12 +0100

    js.jquery?.preloader

--- racetrack.dev/jquery.example.php
+++ racetrack.dev/jquery.example.php
@@ -1,9 +1,10 @@
 <?
 require_once'header.c.php';
+echo"<pre>Jquery is not yet loaded but will .. red background , dashed green header\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n</pre>";
 require_once'footer.c.php';
 ?>
 <script>
 //jquery not yet loaded - wraps anonymous functions, launched once jquery's in
-$(function(){$('#headermenu').css({border:'1px dashed #F07'})});
+$(function(){$('#headermenu').css({border:'3px dashed #0F0'})});
 $(function(){$('body').css({background:'#F00'})});
 </script>
